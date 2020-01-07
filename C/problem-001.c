f97695f4d26a9cdcd4cf54710aba4c3ad5c306a8
Karthick Kumaran <asvkarthick@gmail.com>
2020-01-06 20:39:20 -0800

    Add first program of HackerRank
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ C/problem-001.c
@@ -0,0 +1,28 @@
+/*
+Objective
+In this challenge, we will learn some basic concepts of C that will get you started with the language. You will need to use the same syntax to read input and write output in many C challenges.
+Task
+This challenge requires you to print  on a single line, and then print the already provided input string to stdout.
+Note: You do not need to read any input in this challenge.
+Input Format
+You do not need to read any input in this challenge.
+Output Format
+Print  on the first line, and the string from the given input on the second line.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    printf("Hello, World!\n");
+    printf("%s\n", s);   
+    return 0;
+}
